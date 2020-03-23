bd9490208a9913ed56a800104f5e6cac3560570e
fanghongliang <18292656094@163.com>
2020-03-23 16:33:34 +0800

    [bridge]chore(base.js): 封装base

---
+++ bridgeU/src/utils/base.js
@@ -0,0 +1,45 @@
+import wepy from 'wepy'
+// import qiniuyun from '@/utils/qiniuUploader'
+
+// const baseUrl = 'https://test.****.cn/wx/api//v1'
+const baseUrl = 'https://*****/'
+
+// 上传图片
+const uploadImg = (imageURL, uptokenURL) => {
+    return new Promise((resolve, reject) => {
+        qiniuyun.upload(imageURL, (res) => {
+            resolve(res);
+        }, (error) => {
+            reject(error)
+        }, {
+            region: 'ECN',
+            domain: 'https://images.*****.cn',
+            uptoken: uptokenURL
+        })
+    })
+}
+//请求
+const wxRequest = async (params = {}, url, method,) => {
+    let token = params.token || '';
+        if (params.getToken) {
+            token = wepy.getStorageSync('token')
+        }
+        let res = await wepy.request({
+            url: url,
+            method: method || 'GET',
+            data: params.data || {},
+            header: Object.assign({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `XqCircleToken ${token}`
+            }, params.header || {})
+        });
+    return res
+  }
+
+module.exports = {
+    wxRequest,
+    baseUrl,
+    testbaseUrl,
+    uploadImg,
+};
