b22f45405cd1d7d7cef748d58216bd4e52258e68
fanghongliang <18292656094@163.com>
2020-05-14 20:15:50 +0800

    feat: 添加封装URL携带对象工具函数

---
+++ bridgeU/src/utils/urlHelper.js
@@ -0,0 +1,30 @@
+
+// 构建url
+const buildURL = (url, query = {}, isSequence = true) => {
+    if (!query) return url
+    const joiner = url.match(/\?/) ? '&' : '?'
+    const queryStr = Object.keys(query)
+        .map(key => `${key}=${encodeURIComponent(isSequence ? JSON.stringify(query[key]) : query[key])}`)
+        .join('&')
+    return url + joiner + queryStr
+}
+   
+  // 解析query对象
+const decodeQuery = (originQuery = {}, isSequence = true) => {
+    const result = {}
+    if (!originQuery) return {}
+    return Object.keys(originQuery).reduce((prev, curr) => {
+        result[curr] = decodeURIComponent(originQuery[curr])
+        if (isSequence) {
+            result[curr] = JSON.parse(result[curr])
+        }
+        return result
+    }, result)
+}
+   
+const as = 1
+module.exports = {
+    buildURL,
+    decodeQuery,
+    as,
+}
